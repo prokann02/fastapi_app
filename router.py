b8b0d65513ebc4901f75cf69b9ace3241cbebfc9
prokann <annaaudia5@gmail.com>
2024-04-22 22:26:15 +0300

    Added change product's amount function

--- router.py
+++ router.py
@@ -3,7 +3,7 @@ from typing import Annotated, List
 from fastapi import APIRouter, Depends
 
 from repository import ProductRepository
-from schemas import ProductAdd, Product, ProductId
+from schemas import ProductAdd, Product
 
 router = APIRouter(
     prefix="/products",
@@ -15,9 +15,22 @@ router = APIRouter(
 @router.post("")
 async def add_product(
         product: Annotated[ProductAdd, Depends()],
-) -> ProductId:
+) -> dict:
     product_id = await ProductRepository.add_product(product)
     return {"ok": True, "product_id": product_id}
+
+
+@router.post("{product_id}")
+async def change_product_amount(
+        product_id: int,
+        new_amount: str,
+) -> Product:
+    products = await ProductRepository.find_products()
+
+    current_product = next((product for product in products if product.id == product_id), None)
+    current_product = Product(id=current_product.id, name=current_product.name, amount=new_amount)
+
+    return current_product
 
 
 @router.get("")
